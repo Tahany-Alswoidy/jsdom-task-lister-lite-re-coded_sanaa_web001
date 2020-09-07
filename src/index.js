4bc35451b3d25b1c3dfa5751cf501a375f01e426
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-07 14:20:38 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("submit").addEventListener('click',function(e){
+    e.preventDefault();
+    let task = document.createElement("li");
+    task.innerHTML=document.getElementById("new-task-description").value;
+    document.getElementById("tasks").appendChild(task);
+    document.getElementById("new-task-description").value="";
+  });
 });
