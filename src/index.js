a704b15c726048494dacab093343763caed5f2c3
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-07 14:17:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  document.getElementById.addEventListener()
+  document.getElementById("submit").addEventListener('click',function(e){
+    e.preventDefault();
+    let task = document.createElement("li");
+    task.innerHTML=document.getElementById().value;
+  });
 });
